50b33595800ee2ce6ba1fa92cfbdc4d35b8b9c2a
Developer <dev@example.com>
2026-07-08 14:40:35 +0530

    dev

---
+++ varib.py
@@ -0,0 +1,9 @@
+username=input("Enter your username: ")
+followers=int(input("Enter your number of followers: "))
+verified=input("Are you verified? (True/False): ")
+rating=float(input("Enter your rating (0.0 - 5.0): "))
+
+print(f"username: {username}")
+print(f"followers: {followers}")
+print(f"verified: {verified}")
+print(f"rating: {rating}")
